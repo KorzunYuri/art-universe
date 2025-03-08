d186e01389258d4791e8a21c88b38948078b6ee8
KorzunYuri <korzun.yuri@gmail.com>
2025-03-08 14:49:57 +0100

    cleanup

--- art-universe/art-universe-commons/src/main/java/yurykorzun/art/universe/common/data/raw/apiclient/messaging/ApiCallMessageProducer.java
+++
@@ -1,9 +0,0 @@
-package yurykorzun.art.universe.common.data.raw.apiclient.messaging;
-
-import yurykorzun.art.universe.common.data.raw.apiclient.dto.ApiCallRunRequest;
-
-public interface ApiCallMessageProducer {
-
-    void send(ApiCallRunRequest message);
-
-}
--- music-universe/music-data-raw-lastfm/src/main/java/yurykorzun/art/universe/music/data/raw/lastfm/apiclient/dto/LastfmApiCallCreateResponse.java
+++
@@ -1,8 +0,0 @@
-package yurykorzun.art.universe.music.data.raw.lastfm.apiclient.dto;
-
-import lombok.experimental.SuperBuilder;
-import yurykorzun.art.universe.common.data.raw.apiclient.dto.ApiCallCreateResponse;
-
-@SuperBuilder
-public class LastfmApiCallCreateResponse extends ApiCallCreateResponse {
-}
