858b1ee494f07295e23d6f9b384ac8b719165ec3
KorzunYuri <korzun.yuri@gmail.com>
2025-04-04 22:01:47 +0200

    Lastfm: add LastfmArtistService

---
+++ music-universe/music-data-raw-lastfm/src/main/java/yurykorzun/art/universe/music/data/raw/lastfm/collectable/artist/service/impl/LastfmArtistServiceImpl.java
@@ -0,0 +1,33 @@
+package yurykorzun.art.universe.music.data.raw.lastfm.collectable.artist.service.impl;
+
+import org.springframework.stereotype.Service;
+import yurykorzun.art.universe.music.data.raw.lastfm.collectable.artist.entity.LastfmArtist;
+import yurykorzun.art.universe.music.data.raw.lastfm.collectable.artist.repository.LastfmArtistRepository;
+import yurykorzun.art.universe.music.data.raw.lastfm.collectable.artist.service.LastfmArtistService;
+
+import java.util.List;
+
+@Service
+public class LastfmArtistServiceImpl implements LastfmArtistService {
+
+    private final LastfmArtistRepository artistRepository;
+
+    public LastfmArtistServiceImpl(LastfmArtistRepository artistRepository) {
+        this.artistRepository = artistRepository;
+    }
+
+    @Override
+    public LastfmArtist saveArtist(LastfmArtist artist) {
+        return artistRepository.save(artist);
+    }
+
+    @Override
+    public List<LastfmArtist> saveArtists(List<LastfmArtist> artists) {
+        return artistRepository.saveAll(artists);
+    }
+
+    @Override
+    public List<LastfmArtist> findAllByNames(List<String> names) {
+        return artistRepository.findAllByNameIn(names);
+    }
+}
---
+++ music-universe/music-data-raw-lastfm/src/test/java/yurykorzun/art/universe/music/data/raw/lastfm/collectable/artist/service/impl/LastfmArtistServiceImplTest.java
@@ -0,0 +1,70 @@
+package yurykorzun.art.universe.music.data.raw.lastfm.collectable.artist.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import yurykorzun.art.universe.music.data.raw.lastfm.collectable.artist.entity.LastfmArtist;
+import yurykorzun.art.universe.music.data.raw.lastfm.collectable.artist.repository.LastfmArtistRepository;
+import yurykorzun.art.universe.music.data.raw.lastfm.common.DbConsistencyHelper;
+import yurykorzun.art.universe.music.data.raw.lastfm.common.archetypes.JpaOnlyTest;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@Import(LastfmArtistServiceImpl.class)
+class LastfmArtistServiceImplTest extends JpaOnlyTest {
+    
+    @MockitoBean
+    private LastfmArtistRepository artistRepository;
+
+    @Autowired
+    private DbConsistencyHelper consistencyHelper;
+
+    @Autowired
+    private LastfmArtistServiceImpl artistService;
+
+    @Test
+    void givenArtist_whenSaveArtist_thenRepositorySaveIsCalled() {
+        LastfmArtist artist = consistencyHelper.createArtist();
+        when(artistRepository.save(artist)).thenReturn(artist);
+
+        LastfmArtist savedArtist = artistService.saveArtist(artist);
+
+        assertNotNull(savedArtist);
+        assertEquals(artist, savedArtist);
+        verify(artistRepository, times(1)).save(artist);
+    }
+
+    @Test
+    void givenArtists_whenSaveArtists_thenRepositorySaveAllIsCalled() {
+        List<LastfmArtist> artists = List.of(consistencyHelper.createArtist(), consistencyHelper.createArtist());
+        when(artistRepository.saveAll(artists)).thenReturn(artists);
+
+        List<LastfmArtist> savedArtists = artistService.saveArtists(artists);
+
+        assertNotNull(savedArtists);
+        assertEquals(artists.size(), savedArtists.size());
+        assertEquals(artists, savedArtists);
+        verify(artistRepository, times(1)).saveAll(artists);
+    }
+
+    @Test
+    void givenUrls_whenFindAllByUrls_thenReturnMatchingArtists() {
+        final int artistsNumber = 3;
+        List<String> urls = IntStream.range(0, artistsNumber).mapToObj(i -> UUID.randomUUID().toString()).toList();
+        List<LastfmArtist> artists = urls.stream().map(consistencyHelper::createArtist).toList();
+        when(artistRepository.findAllByNameIn(urls)).thenReturn(artists);
+
+        List<LastfmArtist> foundArtists = artistService.findAllByNames(urls);
+
+        assertNotNull(foundArtists);
+        assertEquals(artists.size(), foundArtists.size());
+        assertEquals(artists, foundArtists);
+        verify(artistRepository, times(1)).findAllByNameIn(urls);
+    }
+}
