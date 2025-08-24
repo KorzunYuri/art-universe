6266ad91cb28e6ed6b3266fcf1f70db34af0db6d
KorzunYuri <korzun.yuri@gmail.com>
2025-08-24 18:19:58 +0200

    Lastfm: add JVM and other system metrics

--- music-universe/music-data-raw-lastfm/src/main/resources/docker/compose/common/grafana-provision.sh
+++ music-universe/music-data-raw-lastfm/src/main/resources/docker/compose/common/grafana-provision.sh
@@ -9,8 +9,9 @@ mkdir -p /etc/grafana/provisioning/datasources
 # Copy dashboard configuration
 cp /tmp/dashboard-templates/dashboard.yml /etc/grafana/provisioning/dashboards/
 
-# Generate dashboard from template
+# Generate dashboards from templates
 envsubst < /tmp/dashboard-templates/lastfm_raw_data_dashboard.json.template > /etc/grafana/provisioning/dashboards/lastfm_raw_data_dashboard.json
+envsubst < /tmp/dashboard-templates/lastfm_system_metrics_dashboard.json.template > /etc/grafana/provisioning/dashboards/lastfm_system_metrics_dashboard.json
 
 # Generate datasource from template
 envsubst < /tmp/datasource-templates/datasource.yml.template > /etc/grafana/provisioning/datasources/datasource.yml
