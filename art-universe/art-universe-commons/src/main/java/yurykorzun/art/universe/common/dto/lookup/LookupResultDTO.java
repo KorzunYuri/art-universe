3f3b3bb614a58420cf6e663b721937b54995e85b
KorzunYuri <korzun.yuri@gmail.com>
2025-09-15 23:00:29 +0200

    music-data-master: fix LookupResultDTO toString() implementation

--- art-universe/art-universe-commons/src/main/java/yurykorzun/art/universe/common/dto/lookup/LookupResultDTO.java
+++ art-universe/art-universe-commons/src/main/java/yurykorzun/art/universe/common/dto/lookup/LookupResultDTO.java
@@ -1,12 +1,10 @@
 package yurykorzun.art.universe.common.dto.lookup;
 
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import yurykorzun.art.universe.common.dto.BaseEntityDto;
 
-@Data
 @SuperBuilder
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
