4494135ace13756d60bb5e9a20e82285038b2f23
KorzunYuri <korzun.yuri@gmail.com>
2025-10-12 20:50:42 +0200

    Lastfm: improve query for track.getInfo method

--- music/data/raw/lastfm/etl/lastfm-calls-generator/src/main/java/yurykorzun/art/universe/music/data/raw/lastfm/collectable/service/LastfmTrackService.java
+++ music/data/raw/lastfm/etl/lastfm-calls-generator/src/main/java/yurykorzun/art/universe/music/data/raw/lastfm/collectable/service/LastfmTrackService.java
@@ -5,13 +5,5 @@ import yurykorzun.art.universe.music.data.raw.lastfm.collectable.entity.LastfmTr
 import java.util.List;
 
 public interface LastfmTrackService {
-
-    /**
-     * Find tracks for track.getInfo API call with the following priority:
-     * 1. Tracks with missing playCount and listenersCount
-     * 2. Prioritize tracks from popular artists (join by artist_id, sort by listenersCount)
-     * 
-     * @return List of tracks to process with track.getInfo API call
-     */
     List<LastfmTrack> findTracksForGetInfo();
 }
