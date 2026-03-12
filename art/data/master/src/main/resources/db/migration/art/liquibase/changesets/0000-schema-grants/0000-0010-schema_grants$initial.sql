6d338c6ca198d8ca937c37332395258064d7ab23
KorzunYuri <korzun.yuri@gmail.com>
2026-03-12 09:05:42 +0100

    fix bootstrap migrations for master-data and master-quiz

--- art/data/master/src/main/resources/db/migration/art/liquibase/changesets/0000-schema-grants/0000-0010-schema_grants$initial.sql
+++ art/data/master/src/main/resources/db/migration/art/liquibase/changesets/0000-schema-grants/0000-0010-schema_grants$initial.sql
@@ -11,6 +11,13 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA art_view GRANT ALL ON FUNCTIONS TO art_dm;
 
 -- Cross-schema: mu_dm reads from art_view.
 -- art_dm is the schema owner and grantor; music-master module cannot issue this grant itself.
-GRANT USAGE ON SCHEMA art_view TO mu_dm;
-GRANT SELECT ON ALL TABLES IN SCHEMA art_view TO mu_dm;
-ALTER DEFAULT PRIVILEGES IN SCHEMA art_view GRANT SELECT ON TABLES TO mu_dm;
+-- Guarded: mu_dm may not exist yet in test environments (no init.sh).
+DO $cross_schema_mu_dm$
+BEGIN
+    IF EXISTS (SELECT FROM pg_roles WHERE rolname = 'mu_dm') THEN
+        GRANT USAGE ON SCHEMA art_view TO mu_dm;
+        GRANT SELECT ON ALL TABLES IN SCHEMA art_view TO mu_dm;
+        ALTER DEFAULT PRIVILEGES IN SCHEMA art_view GRANT SELECT ON TABLES TO mu_dm;
+    END IF;
+END;
+$cross_schema_mu_dm$;
--- music/data/master/src/main/resources/db/migration/mu/liquibase/changesets/0000-schema-grants/0000-0010-schema_grants$initial.sql
+++ music/data/master/src/main/resources/db/migration/mu/liquibase/changesets/0000-schema-grants/0000-0010-schema_grants$initial.sql
@@ -11,5 +11,12 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA mu_view GRANT ALL ON FUNCTIONS TO mu_dm;
 
 -- Cross-schema: mu_quiz_dm reads from mu_view.
 -- mu_dm is the schema owner and grantor; quiz module cannot issue this grant itself.
-GRANT SELECT ON ALL TABLES IN SCHEMA mu_view TO mu_quiz_dm;
-ALTER DEFAULT PRIVILEGES IN SCHEMA mu_view GRANT SELECT ON TABLES TO mu_quiz_dm;
+-- Guarded: mu_quiz_dm may not exist yet in test environments (no init.sh).
+DO $cross_schema_mu_quiz_dm$
+BEGIN
+    IF EXISTS (SELECT FROM pg_roles WHERE rolname = 'mu_quiz_dm') THEN
+        GRANT SELECT ON ALL TABLES IN SCHEMA mu_view TO mu_quiz_dm;
+        ALTER DEFAULT PRIVILEGES IN SCHEMA mu_view GRANT SELECT ON TABLES TO mu_quiz_dm;
+    END IF;
+END;
+$cross_schema_mu_quiz_dm$;
