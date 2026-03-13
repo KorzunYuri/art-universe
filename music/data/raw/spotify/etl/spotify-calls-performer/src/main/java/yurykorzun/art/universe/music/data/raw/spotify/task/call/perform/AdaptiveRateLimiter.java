0f842ab4889178f17a9256f473ba12b64b2db474
KorzunYuri <korzun.yuri@gmail.com>
2026-03-13 21:49:46 +0100

    migrate Spotify calls performer properties to config-service

--- music/data/raw/spotify/etl/spotify-calls-performer/src/main/java/yurykorzun/art/universe/music/data/raw/spotify/task/call/perform/AdaptiveRateLimiter.java
+++ music/data/raw/spotify/etl/spotify-calls-performer/src/main/java/yurykorzun/art/universe/music/data/raw/spotify/task/call/perform/AdaptiveRateLimiter.java
@@ -1,8 +1,10 @@
 package yurykorzun.art.universe.music.data.raw.spotify.task.call.perform;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import yurykorzun.art.universe.common.config.client.ConfigPropertyHolder;
+import yurykorzun.art.universe.music.data.raw.spotify.config.SpotifyPerformerProperty;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -10,25 +12,25 @@ import java.util.concurrent.atomic.AtomicLong;
 @Slf4j
 public class AdaptiveRateLimiter {
 
-    private final long minDelayMs;
-    private final long maxDelayMs;
-    private final double backoffMultiplier;
-    private final long recoveryStepMs;
+    private final ConfigPropertyHolder configPropertyHolder;
 
-    private final AtomicLong currentDelayMs;
+    private long minDelayMs;
+    private long maxDelayMs;
+    private double backoffMultiplier;
+    private long recoveryStepMs;
+    private AtomicLong currentDelayMs;
 
-    public AdaptiveRateLimiter(
-            @Value("${spotify.tasks.calls-perform.rate-limiter.min-delay-ms}") long minDelayMs,
-            @Value("${spotify.tasks.calls-perform.rate-limiter.max-delay-ms}") long maxDelayMs,
-            @Value("${spotify.tasks.calls-perform.rate-limiter.initial-delay-ms}") long initialDelayMs,
-            @Value("${spotify.tasks.calls-perform.rate-limiter.backoff-multiplier}") double backoffMultiplier,
-            @Value("${spotify.tasks.calls-perform.rate-limiter.recovery-step-ms}") long recoveryStepMs
-    ) {
-        this.minDelayMs = minDelayMs;
-        this.maxDelayMs = maxDelayMs;
-        this.backoffMultiplier = backoffMultiplier;
-        this.recoveryStepMs = recoveryStepMs;
-        this.currentDelayMs = new AtomicLong(initialDelayMs);
+    public AdaptiveRateLimiter(ConfigPropertyHolder configPropertyHolder) {
+        this.configPropertyHolder = configPropertyHolder;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.minDelayMs = configPropertyHolder.getInt(SpotifyPerformerProperty.RATE_LIMITER_MIN_DELAY_MS);
+        this.maxDelayMs = configPropertyHolder.getInt(SpotifyPerformerProperty.RATE_LIMITER_MAX_DELAY_MS);
+        this.backoffMultiplier = configPropertyHolder.getDecimal(SpotifyPerformerProperty.RATE_LIMITER_BACKOFF_MULTIPLIER).doubleValue();
+        this.recoveryStepMs = Math.max(50L, minDelayMs / 2);
+        this.currentDelayMs = new AtomicLong(minDelayMs);
     }
 
     public void acquire() throws InterruptedException {
